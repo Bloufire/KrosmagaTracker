b2b63350a267ea97effd8f04859fa7ca6851d63c
PARENT Jean-Marie <parent.jeanmarie@hotmail.com>
2017-09-22 06:14:31 +0200

    commit

--- AddOn Krosmaga - Blou-fire/UIElements/Converters.cs
+++ AddOn Krosmaga - Blou-fire/UIElements/Converters.cs
@@ -14,8 +14,8 @@ namespace AddOn_Krosmaga___Blou_fire.UIElements
 		public object Convert(object value, Type targetType,
 			object parameter, CultureInfo culture)
 		{
-			if ((int) value > 1)
-				return "x" + (int) value;
+			if ((int)value > 1)
+				return "x" + (int)value;
 			else
 				return string.Empty;
 		}
@@ -32,7 +32,7 @@ namespace AddOn_Krosmaga___Blou_fire.UIElements
 		public object Convert(object value, Type targetType,
 			object parameter, CultureInfo culture)
 		{
-			if ((int) value == 0)
+			if ((int)value == 0)
 				return "DEFAITE";
 			else
 				return "VICTOIRE";
@@ -50,7 +50,7 @@ namespace AddOn_Krosmaga___Blou_fire.UIElements
 		public object Convert(object value, Type targetType,
 			object parameter, CultureInfo culture)
 		{
-			if ((bool) value)
+			if ((bool)value)
 				return Visibility.Collapsed;
 			else
 				return Visibility.Visible;
@@ -68,7 +68,7 @@ namespace AddOn_Krosmaga___Blou_fire.UIElements
 		public object Convert(object value, Type targetType,
 			object parameter, CultureInfo culture)
 		{
-			if ((bool) value)
+			if ((bool)value)
 				return "#66000000";
 			else
 				return "Black";
@@ -81,43 +81,49 @@ namespace AddOn_Krosmaga___Blou_fire.UIElements
 		}
 	}
 
-    public class GodClassToColor : IValueConverter
-    {
-        public object Convert(object value, Type targetType,
-            object parameter, CultureInfo culture)
-        {
-            JsonCardsParser.Card val = (JsonCardsParser.Card)value;
-            if (val.Rarity == 4)
-                return "#A5D3E7"; // INFINITE
-            else if (val.Rarity == 3)
-                return "#6840BD"; // KROSMIQUE
-            else
-            {
-                switch (val.GodType)
-                {
-                    case 1:
-                        return "#B02511"; // IOP
-                    case 2:
-                        return "#474D1A"; // CRA
-                    case 3:
-                        return "#C05343"; // ENI
-                    case 4:
-                        return "#78128D"; // ECA
-                    case 5:
-                        return "#A87E00"; // ENU
-                    case 6:
-                        return "#434D67"; // SRAM
-                    case 7:
-                        return "#29387E"; // XEL
-                    case 8:
-                        return "#382F25"; // SACRI
-                    case 10:
-                        return "#848A00"; // SADI
-                    default:
-                        return "#734744"; // NEUTRE
-                }
-            }
-        }
+	public class GodClassToColor : IValueConverter
+	{
+		public object Convert(object value, Type targetType,
+			object parameter, CultureInfo culture)
+		{
+			if (value != null)
+			{
+
+
+				JsonCardsParser.Card val = (JsonCardsParser.Card)value;
+				if (val.Rarity == 4)
+					return "#A5D3E7"; // INFINITE
+				else if (val.Rarity == 3)
+					return "#6840BD"; // KROSMIQUE
+				else
+				{
+					switch (val.GodType)
+					{
+						case 1:
+							return "#B02511"; // IOP
+						case 2:
+							return "#474D1A"; // CRA
+						case 3:
+							return "#C05343"; // ENI
+						case 4:
+							return "#78128D"; // ECA
+						case 5:
+							return "#A87E00"; // ENU
+						case 6:
+							return "#434D67"; // SRAM
+						case 7:
+							return "#29387E"; // XEL
+						case 8:
+							return "#382F25"; // SACRI
+						case 10:
+							return "#848A00"; // SADI
+						default:
+							return "#734744"; // NEUTRE
+					}
+				}
+			}
+			return null;
+		}
 
 		public object ConvertBack(object value, Type targetType, object parameter,
 			System.Globalization.CultureInfo culture)
