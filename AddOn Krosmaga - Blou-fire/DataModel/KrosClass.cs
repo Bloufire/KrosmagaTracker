2a4415895356bf190727f402cd02cdb0976f45cf
PARENT Jean-Marie <parent.jeanmarie@hotmail.com>
2017-10-17 09:14:44 +0200

    Ajout KrosClass + Control de pick de class

---
+++ AddOn Krosmaga - Blou-fire/DataModel/KrosClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AddOn_Krosmaga___Blou_fire.Enums;
+using AddOn_Krosmaga___Blou_fire.Properties;
+
+namespace AddOn_Krosmaga___Blou_fire.DataModel
+{
+    public class KrosClass
+    {
+        public KrosClass()
+        {
+            
+        }
+
+        public KrosClass(string nameClass, string imageURI)
+        {
+            this.NameClass = nameClass;
+            this.ImageUri = imageURI;
+        }
+
+        public string NameClass { get; set; }
+        public string ImageUri { get; set; }
+    }
+
+    public static class KrosClassData
+    {
+        public static List<KrosClass> GetAllClassAndImage()
+        {
+            var ComboClasseValues = Enum.GetValues(typeof(ClassEnum));
+
+            var listToReturn = new List<KrosClass>();
+            listToReturn.Add(new KrosClass("Iop","../../Images/Gods/Iop.png"));
+            listToReturn.Add(new KrosClass("Cra", "../../Images/Gods/Cra.png"));
+            listToReturn.Add(new KrosClass("Eni", "../../Images/Gods/Eni.png"));
+            listToReturn.Add(new KrosClass("Enu", "../../Images/Gods/Enu.png"));
+
+            return listToReturn;
+        }
+    }
+}
---
+++ AddOn Krosmaga - Blou-fire/FlyoutControls/Tracker/PageModel/PickAClassControlPageModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using AddOn_Krosmaga___Blou_fire.DataModel;
+using AddOn_Krosmaga___Blou_fire.Enums;
+using AddOn_Krosmaga___Blou_fire.Helpers;
+using AddOn_Krosmaga___Blou_fire.Models;
+using AddOn_Krosmaga___Blou_fire.Services;
+
+namespace AddOn_Krosmaga___Blou_fire.FlyoutControls.Tracker.PageModel
+{
+    public class PickAClassControlPageModel : ObservableObject
+    {
+        
+        public List<KrosClass> ComboClasseValues { get; set; }
+        public PickAClassControlPageModel()
+        {
+            ComboClasseValues = KrosClassData.GetAllClassAndImage();
+            TrackerSrv.CurrentFiltersStatModel = new FiltersStatModel();
+        }
+
+        #region ClasseSelection
+
+        public ICommand ComboBoxClasseOnSelectionChanged => new RelayCommand(ComboBoxClasse_OnSelectionChangedAction);
+
+        private KrosClass _selectedItemClasse;
+
+        public KrosClass SelectedItemClasse
+        {
+            get => this._selectedItemClasse;
+            set
+            {
+                this._selectedItemClasse = value;
+
+                var v = ClassEnum.TryParse(_selectedItemClasse.NameClass, out ClassEnum result);
+                TrackerSrv.CurrentFiltersStatModel.SelectedClass = result;
+                OnPropertyChanged("SelectedItemClasse");
+            }
+        }
+
+        private void ComboBoxClasse_OnSelectionChangedAction()
+        {
+            if (_selectedItemClasse != null)
+            {
+                Debug.WriteLine("SelectedItemClasse = " + _selectedItemClasse);
+            }
+        }
+
+        #endregion
+
+
+
+
+    }
+}
