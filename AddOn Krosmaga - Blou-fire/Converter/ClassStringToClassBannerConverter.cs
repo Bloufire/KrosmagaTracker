1ef5640750550f164b088ea8fd06b9a655c3978e
Guiver <gael.croibier@gmail.com>
2017-12-14 22:33:33 +0100

    BG, il manque plus qu'a ajouter Images\bg_gods\banner_feca.png

--- AddOn Krosmaga - Blou-fire/Converter/ClassStringToClassBannerConverter.cs
+++ AddOn Krosmaga - Blou-fire/Converter/ClassStringToClassBannerConverter.cs
@@ -16,34 +16,26 @@ namespace AddOn_Krosmaga___Blou_fire.Converter
 			{
 				case "Iop":
 					return "/Images/bg_gods/banner_iop.png";
-					break;					
 				case "Ecaflip":				  
 					return "/Images/bg_gods/banner_eca.png";
-					break;					
 				case "Cra":					 
 					return "/Images/bg_gods/banner_cra.png";
-					break;					
 				case "Eniripsa":			 
 					return "/Images/bg_gods/banner_eni.png";
-					break;					
 				case "Sacrieur":			  
 					return "/Images/bg_gods/banner_sacri.png";
-					break;					
 				case "Sadida":				   
 					return "/Images/bg_gods/banner_sadi.png";
-					break;					
 				case "Sram":				
 					return "/Images/bg_gods/banner_sram.png";
-					break;					
 				case "Xelor":				
 					return "/Images/bg_gods/banner_xel.png";
-					break;					
-				case "Enutrof":				
-					return "/Images/bg_gods/banner_enu.png";
-					break;
-			}
+                case "Enutrof":
+                    return "/Images/bg_gods/banner_enu.png";
+                case "Feca":
+                    return "/Images/bg_gods/banner_feca.png";
+            }
 			return "";
-
 		}
 
 		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
